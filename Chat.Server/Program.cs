4d11a5663176ac5894088720db5a72858ae080b0
Xantrall <bauksutat@mail.ru>
2023-11-20 21:09:34 +0300

    Добавьте файлы проекта.

---
+++ Chat.Server/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var server = new Server();
+await server.Run();
+
+
+public class Server
+{
+    private readonly TcpListener _tcpListener;
+
+    public Server()
+    {
+        _tcpListener = new TcpListener(IPAddress.Any, 7077);
+    }
+
+    public async Task Run()
+    {
+        try
+        {
+            _tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                var tcpClient = await _tcpListener.AcceptTcpClientAsync();
+                var client = new Client(tcpClient, this);
+                Task.Run(client.ProcessAsync);
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    public async Task SendAnswer(Client client, string message)
+    {
+        await client.Writer.WriteAsync(message);
+        await client.Writer.FlushAsync();
+    }
+}
+
+public class Client
+{
+    public Guid Id = Guid.NewGuid();
+    public StreamReader Reader { get; set; }
+    public StreamWriter Writer { get; set; }
+
+    private TcpClient _tcpClient;
+    private Server _server;
+
+    public Client(TcpClient tcpClient, Server server)
+    {
+        _tcpClient = tcpClient;
+        _server = server;
+        Reader = new StreamReader(_tcpClient.GetStream());
+        Writer = new StreamWriter(_tcpClient.GetStream());
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            string userName = await Reader.ReadLineAsync();
+            string message = $"{userName} вошел в чат";
+            await _server.SendAnswer(this, message);
+            Console.WriteLine(message);
+
+            while (true)
+            {
+                try
+                {
+                    message = await Reader.ReadLineAsync();
+
+                    if (message == null)
+                        continue;
+
+                    message = $"{userName}: {message}";
+                    Console.WriteLine(message);
+                    await _server.SendAnswer(this, message);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        catch
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Sockets;
+
+var host = "127.0.0.1";
+int port = 7077;
+using var tcpClient = new TcpClient();
+Console.WriteLine("Введите свое имя:");
+var name = Console.ReadLine();
+StreamWriter writer = null;
+StreamReader Reader = null;
+
+try
+{
+    tcpClient.Connect(host, port);
+    writer = new StreamWriter(tcpClient.GetStream());
+    Reader = new StreamReader(tcpClient.GetStream());
+
+    if (writer == null)
+        return;
+
+    Task.Run(() => ReceiveMessageAsync(Reader));
+    await SendMessageAsync(writer);
+}
+catch
+{
+
+}
+
+async Task ReceiveMessageAsync(StreamReader reader)
+{
+    while (true)
+    {
+        try
+        {
+            var message = await reader.ReadLineAsync();
+
+            if (string.IsNullOrEmpty(message))
+                continue;
+
+            Print(message);
+        }
+        catch
+        {
+
+        }
+    }
+}
+
+void Print(string message)
+{
+    Console.WriteLine(message);
+}
+
+
+async Task SendMessageAsync(StreamWriter writer)
+{
+    await writer.WriteLineAsync(name);
+    await writer.FlushAsync();
+    Console.WriteLine("Введите сообщение:");
+
+    while (true)
+    {
+        var message = Console.ReadLine();
+        await writer.WriteLineAsync(message);
+        await writer.FlushAsync();
+    }
+}
